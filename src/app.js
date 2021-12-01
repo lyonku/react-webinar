b943e92583c99f277827140fda6e5b2dfdb4debb
Zhenya Pilipenko <z.pilipenko123@gmail.com>
2021-12-01 20:11:47 +0300

    Improve some code

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import Modal from './components/Modal';
  * @param store {Store} Состояние с действиями
  */
 function App({store}) {
-  const [active, setActive] = useState(false)
+  // const [active, setActive] = useState(false)
 
   const callbacks = {
     onCreateItem: useCallback(() => store.createItem(), [store]),
@@ -20,14 +20,11 @@ function App({store}) {
   return (
     <Layout head={<h1>Магазин</h1>}>
       <Controls basket={store.getState().basket} 
-                onCreate={callbacks.onCreateItem} 
-                setActive={setActive}/>
+                onCreate={callbacks.onCreateItem}/>
       <List items={store.getState().items}
             onSelectItem={callbacks.onSelectItem}
             onAppendItem={callbacks.onAppendItem}/>
-      <Modal basket={store.getState().basket}
-            active={active} 
-            setActive={setActive}/>
+      
     </Layout>
   );
 }
--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useCallback, useState} from 'react';
 import propTypes from 'prop-types';
 import './styles.css';
 import plural from 'plural-ru';
+import Modal from '/components/Modal';
 
 
-function Controls({setActive, basket}){
+function Controls({basket}){
+  const [active, setActive] = useState(false)
+
   const totalPrice = basket.reduce((sum, current) => sum + current.price * current.count, 0);
   const totalCount = basket.reduce((sum, current) => sum + current.count, 0);
   const wordDeclination = plural(totalCount, ' товар', ' товара', ' товаров');
 
+  const callbacks = {
+    onClick: useCallback(() => {
+      setActive(true) 
+  }, [setActive, basket])};
+
   return <div className='Controls'>
     <div className='Controls__basket'>В корзине: {totalCount == 0 ? <span>Пусто</span>: <span>{totalCount} {wordDeclination} / {new Intl.NumberFormat().format(totalPrice)} &#8381; </span>}</div>
-    <button onClick={() => setActive(true) }> Перейти</button>
+    <button onClick={callbacks.onClick}> Перейти</button>
+    <Modal basket={basket}
+            active={active} 
+            setActive={setActive}/>
   </div>
   
 }
