b577130b549967fee65c1a9f255c95a57126163e
Zhenya Pilipenko <z.pilipenko123@gmail.com>
2021-12-29 17:34:19 +0300

    Reworked textarea component

--- src/components/textarea/index.js
+++ src/components/textarea/index.js
@@ -2,26 +2,14 @@ import React, {useCallback, useEffect, useState} from 'react';
 import propTypes from "prop-types";
 import {cn} from '@bem-react/classname'
 import './styles.css';
-import throttle from "lodash.throttle";
 
 function Textarea(props) {
 
-  // Внутренний стейт по умолчанию с переданным value
-  const [value, change] = useState(props.value);
 
-  // Задержка для вызова props.onChange
-  const changeThrottle = useCallback(throttle(value => props.onChange(value), 1000), [props.onChange]);
-
-  // Обработчик изменений в поле
   const onChange = useCallback(event => {
-    change(event.target.value);
-    changeThrottle(event.target.value);
-  }, [change, changeThrottle]);
+    props.onChange(event.target.value);
+  }, [props.onChange]);
 
-  // Обновление стейта, если передан новый value
-  useEffect(() => {
-    change(props.value);
-  }, [props.value]);
 
   // CSS классы по БЭМ
   const className = cn('Input');
@@ -29,9 +17,7 @@ function Textarea(props) {
   return (
     <textarea
       className={className({theme: props.theme})}
-      value={value}
-      type={props.type}
-      placeholder={props.placeholder}
+      value={props.value}
       onChange={onChange}
     />
   )
@@ -39,16 +25,12 @@ function Textarea(props) {
 
 Textarea.propTypes = {
   value: propTypes.string,
-  type: propTypes.string,
-  placeholder: propTypes.string,
   onChange: propTypes.func,
-  theme: propTypes.string,
 }
 
 Textarea.defaultProps = {
   onChange: () => {},
-  type: 'text',
-  theme: ''
+  value: ''
 }
 
 export default React.memo(Textarea);
